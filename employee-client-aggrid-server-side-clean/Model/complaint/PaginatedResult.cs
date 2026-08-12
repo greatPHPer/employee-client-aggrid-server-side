98c7549f0747fda5b70f94b654c02d1550198313
greatPHPer <dhilip007.india@gmail.com>
2026-08-12 16:23:13 +0530

    Delete employee-client-aggrid-server-side-clean directory
    only ecesary codsneeded

--- employee-client-aggrid-server-side-clean/Model/complaint/PaginatedResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Employee_Client.Api.Model.complaint
-{
-    // 1. Add this helper class at the bottom of AuthController.cs (or in your Models folder)
-    public class PaginatedResult<T>
-    {
-        public List<T> Data { get; set; } = new();
-        public int TotalRecords { get; set; }
-    }
-}
